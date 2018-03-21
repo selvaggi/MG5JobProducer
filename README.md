60c454c9b1ec5ba475513b5b324a8643c5dedc6e
Michele Selvaggi <michele.selvaggi@uclouvain.be>
2018-03-21 18:12:15 +0100

    first meaningful commit

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# MG5JobProducer
+[]() MG5JobProducer
+====================
+
+Wraps MG5 and sends jobs on the LSF cluster. Examples:
+
+```
+python bin/submitMGjobs.py --mg5card examples/pp_ee.mg5 --outdir ./testLSF --procname pp_ee --njobs 10 --nev 100 --queue 8nh
+```
+
+Or with an external model:
+
+```
+python bin/submitMGjobs.py --mg5card examples/pp_hh.mg5 --model models/loop_sm_hh.tar --outdir ./test_hh --procname pp_hh --njobs 10 --nev 100 --queue 8nh
+```
---
+++ bin/submitMGjobs.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+import os, sys, subprocess
+import argparse
+import commands
+import time
+#____________________________________________________________________________________________________________
+### processing the external os commands
+def processCmd(cmd, quite = 0):
+    status, output = commands.getstatusoutput(cmd)
+    if (status !=0 and not quite):
+        print 'Error in processing command:\n   ['+cmd+']'
+        print 'Output:\n   ['+output+'] \n'
+    return output
+
+#_____________________________________________________________________________________________________________
+def main():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument ('--mg5card', help='mg5 config file',  default='card.mg5')
+    parser.add_argument ('--cutfile', help='cuts.f file for additional cuts',  default='cuts.f')
+    parser.add_argument ('--model', help='model tarball',  default='model.tgz')
+    parser.add_argument ('--outdir', help='output directory e.g. /eos/experiment/fcc/hh/generation/process', default='.')
+    parser.add_argument ('--procname', help='process name', default='')
+    parser.add_argument ('--njobs', help='number of jobs ', type=int, default=2)
+    parser.add_argument ('--nev', help='number of events per job', type=int, default=10000)
+    parser.add_argument ('--queue', help='master lsf queue', default='8nh')
+    parser.add_argument ('--memory', help='min virtual memory', default='4000')
+    parser.add_argument ('--disk', help='min disk space',       default='2000')
+
+    args = parser.parse_args()
+
+    outdir = os.path.abspath(args.outdir)
+    mg5card = os.path.abspath(args.mg5card)
+    cuts = os.path.abspath(args.cutfile)
+    model = os.path.abspath(args.model)
+
+    jobsdir = './BatchOutput/' + args.procname
+
+    if not os.path.exists(jobsdir):
+       os.makedirs(jobsdir)
+       os.makedirs(jobsdir+'/std/')
+       os.makedirs(jobsdir+'/cfg/')
+    else:
+       sys.exit('Output dir: "'+jobsdir+'" exists.')
+
+    print '[Submitting jobs]'
+    jobCount=0
+    
+    for job in xrange(args.njobs):
+
+       seed = str(job)
+
+       print 'Submitting job '+str(job)+' out of '+str(args.njobs)
+       basename =  args.procname+ '_'+seed
+       
+       cmd = 'bsub -o '+jobsdir+'/std/'+basename +'.out -e '+jobsdir+'/std/'+basename +'.err -q '+args.queue
+       cmd += ' -R "rusage[mem={}:pool={}]"'.format(args.memory,args.disk)
+       cmd +=' -J '+basename+' "bin/submitMG.sh '+mg5card+' '+args.procname+' '+outdir+' '+seed+' '+str(args.nev)+' '+cuts+' '+model+'"'
+       
+       print cmd
+       
+       # submitting jobs
+       
+       output = processCmd(cmd)
+       while ('error' in output):
+           time.sleep(1.0);
+           output = processCmd(cmd)
+           if ('error' not in output):
+               print 'Submitted after retry - job '+str(jobCount+1)
+
+       jobCount += 1
+
+#_______________________________________________________________________________________
+if __name__ == "__main__":
+    main()
+
